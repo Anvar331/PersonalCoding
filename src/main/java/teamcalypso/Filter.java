f98bfb9b731bd3903643b22a318d596a7ee32eaf
Anvar331 <anvar.intezar@gmail.com>
2020-09-23 20:40:48 +0300

    Filter functionality

---
+++ src/main/java/teamcalypso/Filter.java
@@ -0,0 +1,14 @@
+package teamcalypso;
+
+/*public class Filter<E extends Storage>  {
+
+    private final E[] data;
+
+    public Filter(E[] data) {
+        this.data = data;
+    }
+
+    public E[] applyFilter() {
+
+    }
+}*/
---
+++ src/test/java/teamcalypso/FilterTest.java
@@ -0,0 +1,21 @@
+package teamcalypso;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilterTest {
+
+    @Test
+    public void firstFilterTest() {
+        Storage[] data = new Storage[]{
+                new Storage(1, "int", 10),
+                new Storage(4, "String", 12),
+        };
+        Filter firstFilter = new Filter(data);
+        Storage[] array = firstFilter.applyFilter();
+        assertEquals(array.length,1);
+        assertEquals(array[0].getType(),"int");
+    }
+
+}
