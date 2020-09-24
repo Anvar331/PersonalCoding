b4de3c09291d34a23ec71532eb0f5defe4a8019e
Anvar331 <anvar.intezar@gmail.com>
2020-09-24 20:38:41 +0300

    Filter functionality Tests added

--- src/main/java/teamcalypso/Filter.java
+++ src/main/java/teamcalypso/Filter.java
@@ -16,13 +16,13 @@ public class Filter<E extends Storage>  {
 
     private E[] findStorageWithTypesWithCapitalLetters() {
         int index = 0;
-        Storage[] newStorage = new Storage[data.length];
+        E[] newStorage = new E [data.length];
         for (int i = 0; i < data.length; i++) {
             if ((data[i].getType().charAt(0) > 'a') && (data[i].getType().charAt(0) < 'z')){
                  newStorage[index] = data[i];
                  index++;
             }
         }
-        return data;
+        return newStorage;
     }
 }
--- src/test/java/teamcalypso/FilterTest.java
+++ src/test/java/teamcalypso/FilterTest.java
@@ -29,4 +29,50 @@ public class FilterTest {
         assertEquals(array.length,0);
     }
 
+    @Test
+    public void thirdFilterTest() {
+        Storage[] data = new Storage[]{
+                new Storage(3, "double", 6),
+                new Storage(4, "String", 12),
+                new Storage(5, "BigDecimal", 13),
+                new Storage(6, "Integer", 12),
+                new Storage(7, "Double", 15),
+                new Storage(8, "Object", 4),
+        };
+        Filter firstFilter = new Filter(data);
+        Storage[] array = firstFilter.applyFilter();
+        assertEquals(array.length,1);
+        assertEquals(array[0].getType(),"int");
+    }
+
+    @Test
+    public void fourthFilterTest() {
+        Storage[] data = new Storage[]{
+                new Storage(1, "int", 10),
+                new Storage(2, "float", 4),
+                new Storage(3, "double", 6),
+                new Storage(4, "String", 12),
+        };
+        Filter firstFilter = new Filter(data);
+        Storage[] array = firstFilter.applyFilter();
+        assertEquals(array.length,3);
+        assertEquals(array[0].getType(),"int");
+        assertEquals(array[1].getType(),"float");
+        assertEquals(array[2].getType(),"double");
+    }
+
+    @Test
+    public void fifthFilterTest() {
+        Storage[] data = new Storage[]{
+                new Storage(1, "int", 10),
+                new Storage(2, "float", 4),
+                new Storage(3, "double", 6),
+        };
+        Filter firstFilter = new Filter(data);
+        Storage[] array = firstFilter.applyFilter();
+        assertEquals(array.length,3);
+        assertEquals(array[0].getType(),"int");
+        assertEquals(array[1].getType(),"float");
+        assertEquals(array[2].getType(),"double");
+    }
 }
