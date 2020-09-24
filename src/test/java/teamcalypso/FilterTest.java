4053d4f589d8446b031a3f0703de3c574f7fdabd
Anvar331 <anvar.intezar@gmail.com>
2020-09-24 07:44:47 +0300

    Filter functionality Corrections4

--- src/test/java/teamcalypso/FilterTest.java
+++ src/test/java/teamcalypso/FilterTest.java
@@ -15,6 +15,18 @@ public class FilterTest {
         Filter firstFilter = new Filter(data);
         Storage[] array = firstFilter.applyFilter();
         assertEquals(array.length,1);
+        assertEquals(array[0].getType(),"int");
+    }
+
+    @Test
+    public void secondFilterTest() {
+        Storage[] data = new Storage[]{
+                new Storage(8, "Double", 15),
+                new Storage(4, "String", 12),
+        };
+        Filter firstFilter = new Filter(data);
+        Storage[] array = firstFilter.applyFilter();
+        assertEquals(array.length,0);
     }
 
 }
